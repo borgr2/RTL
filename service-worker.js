0ea0cbb0ac7e0063eb223e5d80f864b2ea0446f6
borgr2 <sabangrenan@gmail.com>
2025-07-21 21:11:43 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,39 @@
+// service-worker.js
 const CACHE_NAME = 'chicken-inventory-cache-v1';
-const urlsToCache = [
+const URLS_TO_CACHE = [
   '/',
   '/index.html',
   '/script.js',
-  // Add more assets if needed
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  // Add any other assets you want cached
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
